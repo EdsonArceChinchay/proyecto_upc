cc8fba3084726d4523d69edc704155170afc498e
Cristhian Raúl Solis Aquino <cristhianraul.solisaquino@everis.nttdata.com>
2022-05-25 22:34:50 +0000

    AltaFijaAltaMovilCallCenterPage.java edited online with Bitbucket

--- src/test/java/com/tdp/ct/web/page/AltaFijaAltaMovilCallCenterPage.java
+++ src/test/java/com/tdp/ct/web/page/AltaFijaAltaMovilCallCenterPage.java
@@ -189,6 +189,8 @@ public class AltaFijaAltaMovilCallCenterPage extends WebBase {
         UtilWeb.waitForSeconds(4);
         JavascriptExecutor js = (JavascriptExecutor)driver();
         js.executeScript("window.scrollTo(document.body.scrollHeight,0)");
+        UtilWeb.waitForSeconds(1);
+        js.executeScript("window.scrollTo(document.body.scrollHeight,150)");
     }
 
     public void oferta(){
