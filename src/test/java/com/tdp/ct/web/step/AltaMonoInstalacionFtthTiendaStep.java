ccd6c25c679d1c9f1e69e19f9049d0560ea67210
Edith Veronica Maturrano Roman <ematurra@emeal.nttdata.com>
2022-06-02 15:01:44 -0500

    update: Alta mono instalacion FTTH

--- src/test/java/com/tdp/ct/web/step/AltaMonoInstalacionFtthTiendaStep.java
+++ src/test/java/com/tdp/ct/web/step/AltaMonoInstalacionFtthTiendaStep.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AltaMonoInstalacionHfcTiendaStep {
+public class AltaMonoInstalacionFtthTiendaStep {
 
     @Autowired
     private StepPages page;
