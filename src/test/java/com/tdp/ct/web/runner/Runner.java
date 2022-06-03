89c256a4643c5d475468a9fdcfe0754edf144f1b
Carlos Miguel Vicente Padilla <cvicentp@emeal.nttdata.com>
2022-06-03 17:37:47 -0500

    flujos terminados

--- src/test/java/com/tdp/ct/web/runner/Runner.java
+++ src/test/java/com/tdp/ct/web/runner/Runner.java
@@ -16,7 +16,7 @@ import java.util.logging.Logger;
         features = {"src/test/resources/features"},
         glue = {"com.tdp.ct.web.hooks", "com.tdp.ct.web.glue"},
 
-        tags = "@AltaMovilPostPagoDeliveryCallCenter_HAPPYPATH"
+        tags = "@AltaFijaMovilComboProactivo_HappyPath"
 
 )
 public class Runner {
