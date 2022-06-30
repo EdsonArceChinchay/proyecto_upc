baf2746db02dddcc79fc9ad1a3ffb33a3da56137
Cristhian Raúl Solis Aquino <cristhianraul.solisaquino@everis.nttdata.com>
2022-06-30 19:53:08 +0000

    AltaMovilPostpagoCallCenterPage.java edited online with Bitbucket

--- src/test/java/com/tdp/ct/web/page/AltaMovilPostpagoCallCenterPage.java
+++ src/test/java/com/tdp/ct/web/page/AltaMovilPostpagoCallCenterPage.java
@@ -137,7 +137,10 @@ public class AltaMovilPostpagoCallCenterPage extends WebBase {
     }
 
     public void doyClickEnIniciarRegistro() {
-        js().scrollElementTop(btnIniciar);
+        UtilWeb.waitForSeconds(1);
+        JavascriptExecutor js = (JavascriptExecutor)driver();
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+        //js().scrollElementTop(btnIniciar);
         UtilWeb.waitForSeconds(2);
         click(btnIniciar, 10);
         System.out.println("paso por aqui" + btnIniciar.getText());
