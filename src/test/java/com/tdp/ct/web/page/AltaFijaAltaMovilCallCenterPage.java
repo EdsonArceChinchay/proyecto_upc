d1ca1821f8007fba5cfef0fbed40e661f4e7d13e
Carlos Miguel Vicente Padilla <cvicentp@emeal.nttdata.com>
2022-08-05 17:45:29 -0500

    FE CARLOS REGRESION

--- src/test/java/com/tdp/ct/web/page/AltaFijaAltaMovilCallCenterPage.java
+++ src/test/java/com/tdp/ct/web/page/AltaFijaAltaMovilCallCenterPage.java
@@ -204,7 +204,7 @@ public class AltaFijaAltaMovilCallCenterPage extends WebBase {
     }
 
     public void scrollUp(){
-        UtilWeb.waitForSeconds(50);
+        UtilWeb.waitForSeconds(100);
         JavascriptExecutor js = (JavascriptExecutor)driver();
         js.executeScript("window.scrollTo(document.body.scrollHeight,0)");
         js.executeScript("window.scrollTo(document.body.scrollHeight,150)");
