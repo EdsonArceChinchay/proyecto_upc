7d003f8c148d6f5b2c2be2f3d3f0e59183f0b544
Carlos Miguel Vicente Padilla <cvicentp@emeal.nttdata.com>
2022-08-24 11:51:57 -0500

    FE FLUJO ALTA MOVIL EQUIPO _CARLOS V

--- src/test/java/com/tdp/ct/web/page/StepPages.java
+++ src/test/java/com/tdp/ct/web/page/StepPages.java
@@ -27,5 +27,5 @@ public class StepPages{
     public AltaFijaTiendaUpgradeDecoHDalquilerADecoSmartHDAlquilerPage altaFijaTiendaUpgradeDecoHDalquilerADecoSmartHDAlquilerPage(){return new AltaFijaTiendaUpgradeDecoHDalquilerADecoSmartHDAlquilerPage();}
     public MigraTrioaMTcanalCallCenterPage migraTrioaMTcanalCallCenterPage(){return new MigraTrioaMTcanalCallCenterPage();}
     public AltaMovilControlFinancimientoPage altaMovilControlFinancimientoPage(){return new AltaMovilControlFinancimientoPage();}
-
+    public AltaMovilSVATiendaPage altaMovilSVATiendaPage(){return new AltaMovilSVATiendaPage();}
 }
