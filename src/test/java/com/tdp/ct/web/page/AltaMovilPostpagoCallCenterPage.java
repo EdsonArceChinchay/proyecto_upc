d9c82cb7adb5dce3b5a11aab461db81a0030f0a2
Maria Paula Fariba Sanchez Rapre <msanchra@emeal.nttdata.com>
2022-09-08 19:43:29 -0500

    FE-MARIASANCHEZ

--- src/test/java/com/tdp/ct/web/page/AltaMovilPostpagoCallCenterPage.java
+++ src/test/java/com/tdp/ct/web/page/AltaMovilPostpagoCallCenterPage.java
@@ -143,7 +143,7 @@ public class AltaMovilPostpagoCallCenterPage extends WebBase {
     }
 
     public void doyClickEnIniciarRegistro() {
-        UtilWeb.waitForSeconds(3);
+        UtilWeb.waitForSeconds(8);//3
         JavascriptExecutor js = (JavascriptExecutor)driver();
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
         //js().scrollElementTop(btnIniciar);
