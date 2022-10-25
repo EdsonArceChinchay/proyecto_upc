7719458edc046fe0c6cb30b664e8aafcbd428176
Maria Paula Fariba Sanchez Rapre <msanchra@emeal.nttdata.com>
2022-10-25 11:26:13 -0500

    FE-25/10/2022 - 2

--- src/test/java/com/tdp/ct/web/page/AltaFijaAltaMovilCallCenterPage.java
+++ src/test/java/com/tdp/ct/web/page/AltaFijaAltaMovilCallCenterPage.java
@@ -194,6 +194,7 @@ public class AltaFijaAltaMovilCallCenterPage extends WebBase {
 //        modalError(3,btnReintentar,"Click al elemento Reitentar");
 //        modalError(3,btnReintentar,"Click al elemento Reitentar");
 //        modalError(3,btnReintentar,"Click al elemento Reitentar");
+        UtilWeb.waitForSeconds(10);//
         waitUntilElementIsVisible(esperarCorreo, 50);
         UtilWeb.waitForSeconds(3);
         WebElement correoElectronico = find().getElementByXPath("(//div[@class='cont-input-icon mb-20 tdp-col-lg-7 tdp-col-12'])[1]/tdp-st-input-text");
