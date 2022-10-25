da2f183764da9b4a09b8ef843a3ec2fbae75c457
Edson Hafid Arce Chinchay <earcechi@emeal.nttdata.com>
2022-10-25 11:41:29 -0500

    Flujo altaMovilControl_Equipo_canalCallCenter culminado

--- src/test/java/com/tdp/ct/web/runner/Runner.java
+++ src/test/java/com/tdp/ct/web/runner/Runner.java
@@ -16,7 +16,7 @@ import java.util.logging.Logger;
         stepNotifications = true,
         features = {"src/test/resources/features"},
         glue = {"com.tdp.ct.web.hooks", "com.tdp.ct.web.glue"},
-        tags = "@CambioVelocidadDuo50Mb+SvaRepetidorCallCenter+CambioDirección"
+        tags = "@AltaCEXControlEquipoLineaCallCenter"
 )
 
 class Runner {
