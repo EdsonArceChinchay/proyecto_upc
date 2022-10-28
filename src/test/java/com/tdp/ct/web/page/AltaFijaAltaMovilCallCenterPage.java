1f3311c773e03a9550f696da603d1ff9e58889aa
Angel Antonio Medina Flores <amedinfl@emeal.nttdata.com>
2022-10-28 10:49:50 -0500

    MigracionUpsellduoaTrio

--- src/test/java/com/tdp/ct/web/page/AltaFijaAltaMovilCallCenterPage.java
+++ src/test/java/com/tdp/ct/web/page/AltaFijaAltaMovilCallCenterPage.java
@@ -229,7 +229,7 @@ public class AltaFijaAltaMovilCallCenterPage extends WebBase {
     }
 
     public void scrollUp() {
-        UtilWeb.waitForSeconds(8);//10
+        UtilWeb.waitForSeconds(20);//10
         JavascriptExecutor js = (JavascriptExecutor) driver();
         js.executeScript("window.scrollTo(document.body.scrollHeight,0)");
         js.executeScript("window.scrollTo(document.body.scrollHeight,150)");
