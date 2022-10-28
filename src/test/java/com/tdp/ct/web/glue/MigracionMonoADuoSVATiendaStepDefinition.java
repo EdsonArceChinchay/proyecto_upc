793028c67b9a22f456b5c418691d09cf43f15152
Ever Alexander Villanueva Guerreros <evillang@emeal.nttdata.com>
2022-10-28 17:49:38 -0500

    Migracion de Mono a Duo

--- src/test/java/com/tdp/ct/web/glue/MigracionMonoADuoSVATiendaStepDefinition.java
+++ src/test/java/com/tdp/ct/web/glue/MigracionMonoADuoSVATiendaStepDefinition.java
@@ -28,4 +28,10 @@ public class MigracionMonoADuoSVATiendaStepDefinition {
     }
 
 
+    @Y("doy clic en agregar sva {string}")
+    public void doyClicEnAgregarSva(String Bloques) {
+        MigracionMonoADuoSVATiendaStep.clicEnAgregarSVA();
+        MigracionMonoADuoSVATiendaStep.agregoSVATV(Bloques);
+        MigracionMonoADuoSVATiendaStep.clicGuardarCambios();
+    }
 }
