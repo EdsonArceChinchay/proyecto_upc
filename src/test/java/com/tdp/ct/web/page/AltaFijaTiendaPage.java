4fc11a3e9be068bd20f4bd40b3bb37a076af567a
Carlos Miguel Vicente Padilla <cvicentp@emeal.nttdata.com>
2023-02-03 09:50:47 -0500

    FE CARLOS CORRECCION

--- src/test/java/com/tdp/ct/web/page/AltaFijaTiendaPage.java
+++ src/test/java/com/tdp/ct/web/page/AltaFijaTiendaPage.java
@@ -62,10 +62,10 @@ public class AltaFijaTiendaPage extends WebBase {
     }
 
     public void listaPlanFija(String planFija) {
-        modalError(3,btnReintentar,"Click al elemento Reitentar");
-        modalError(5,btnReintentar,"Click al elemento Reitentar");
-        modalError(3,btnReintentar,"Click al elemento Reitentar");
-        waitUntilElementIsVisible(esperalistaPlanFija, 10);
+//        modalError(3,btnReintentar,"Click al elemento Reitentar");
+//        modalError(5,btnReintentar,"Click al elemento Reitentar");
+//        modalError(3,btnReintentar,"Click al elemento Reitentar");
+       waitUntilElementIsVisible(esperalistaPlanFija, 5);
         UtilWeb.waitForSeconds(5);
         clickElementInAList(listaPlanFija, planFija);
         UtilWeb.waitForSeconds(5);
