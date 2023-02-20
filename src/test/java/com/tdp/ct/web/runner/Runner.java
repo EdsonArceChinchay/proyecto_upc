c41eaf7ca26c4dd33039f107e626979accf5ddc3
Eloy Silverio Milla Mendoza <emillame@emeal.nttdata.com>
2023-02-20 14:30:49 -0500

    update Runner

--- src/test/java/com/tdp/ct/web/runner/Runner.java
+++ src/test/java/com/tdp/ct/web/runner/Runner.java
@@ -16,12 +16,8 @@ import java.util.logging.Logger;
         stepNotifications = true,
         features = {"src/test/resources/features"},
         glue = {"com.tdp.ct.web.hooks", "com.tdp.ct.web.glue"},
-
-<<<<<<< HEAD
-        tags = "@CambiodeplanmovilCEcallcenter"
-=======
         tags = "@AltaFijaTienda"
->>>>>>> bca55895c1383ce1019f021f30a82b9c99c1f72d
+
 
 )
 
