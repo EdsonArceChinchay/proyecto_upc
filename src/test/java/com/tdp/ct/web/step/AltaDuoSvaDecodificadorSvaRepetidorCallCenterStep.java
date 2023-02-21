3ba64b069733fb3f6e179f4b91dd7e530da66946
Maria Paula Fariba Sanchez Rapre <msanchra@emeal.nttdata.com>
2023-02-21 12:27:42 -0500

    FE-21/02/2023

--- src/test/java/com/tdp/ct/web/step/AltaDuoSvaDecodificadorSvaRepetidorCallCenterStep.java
+++ src/test/java/com/tdp/ct/web/step/AltaDuoSvaDecodificadorSvaRepetidorCallCenterStep.java
@@ -1,20 +1,21 @@
 package com.tdp.ct.web.step;
-
 import com.tdp.ct.web.page.StepPages;
 import com.tdp.ct.web.service.aspect.evidence.ScreenShotAfter;
+import com.tdp.ct.web.service.aspect.evidence.ScreenShotBefore;
 import com.tdp.ct.web.service.util.UtilWeb;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AltaDuoSvaDecodificadorSvaRepetidorCallCenterStep {
 
     @Autowired
-    private StepPages page;
+    private StepPages pages;
 
+    @ScreenShotBefore
     @ScreenShotAfter
-    public void seleccionardecodificador(String string){
-        System.out.println("segundo paso");
+    public void seleccionar_deco(String string){
         UtilWeb.waitForSeconds(3);
-        page.AltaDuoSvaDecodificadorSvaRepetidorCallCenterPage().seleccionardecodificador(string);
-
+        pages.AltaDuoSvaDecodificadorSvaRepetidorCallCenterPage().seleccionardecodificador(string);
     }
 }
