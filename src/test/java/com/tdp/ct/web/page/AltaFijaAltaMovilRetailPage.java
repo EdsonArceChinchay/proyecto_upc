5209ecbecca7b7d3a3ecc46a44eaa4d32d45f4d4
Eloy Silverio Milla Mendoza <emillame@emeal.nttdata.com>
2023-02-28 17:53:59 -0500

    alta trio upfront retail

--- src/test/java/com/tdp/ct/web/page/AltaFijaAltaMovilRetailPage.java
+++ src/test/java/com/tdp/ct/web/page/AltaFijaAltaMovilRetailPage.java
@@ -18,7 +18,7 @@ public class AltaFijaAltaMovilRetailPage extends WebBase {
     @FindBy(css = ".tdp-col-sm-2:nth-child(2) .stl-movil")
     protected WebElement btnMovil;
 
-    @FindBy(xpath = "//div[@class=\"show-offerts\"]/button")
+        @FindBy(xpath = "//div[@class=\"show-offerts\"]/button")
     protected WebElement btnMostrar;
 
     @FindBy(xpath = "//div[1]/tdp-st-card[1]/div/div[2]/form/div[6]/div/button")
