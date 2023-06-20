30bf32112f6a7622a26486b79efc3d04a105a279
Jose Moises Llamoca Chucas <jllamoca@emeal.nttdata.com>
2023-06-20 13:16:43 -0500

    flujos moises - cambios en alta trio mt sva

--- src/test/java/com/tdp/ct/web/runner/Runner.java
+++ src/test/java/com/tdp/ct/web/runner/Runner.java
@@ -16,16 +16,9 @@ import java.util.logging.Logger;
         stepNotifications = true,
         features = {"src/test/resources/features"},
         glue = {"com.tdp.ct.web.hooks", "com.tdp.ct.web.glue"},
-<<<<<<< HEAD
         tags = "@AltatrioMT_SVA"
 
 
-
-
-
-=======
-        tags = "@MigracionDuoADuoSVaCallcenter"
->>>>>>> cff907d3c66c3a47f35e1d34715343e0bced1e60
 )
 
 
