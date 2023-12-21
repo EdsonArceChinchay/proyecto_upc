a7e2e6df14af9b9e5bb73aa82b71286786109ad6
Andres Michilot Shimazu <amichilo@emeal.nttdata.com>
2023-12-21 14:26:58 -0500

    Mensaje de confirmación

--- src/test/java/com/tdp/ct/web/runner/Runner.java
+++ src/test/java/com/tdp/ct/web/runner/Runner.java
@@ -16,7 +16,7 @@ import java.util.logging.Logger;
         stepNotifications = true,
         features = {"src/test/resources/features"},
         glue = {"com.tdp.ct.web.hooks", "com.tdp.ct.web.glue"},
-        tags = "@AltaDuoSvaRetail"
+        tags = "@MigracionMTaMT_Equipo_SVA_Tiendas"
 )
 
 class Runner {
